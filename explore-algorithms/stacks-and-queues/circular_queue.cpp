686041e714c676d36233f07f780e2575f86dcab9
Bee-Mar <bpmarlowe-software@protonmail.com>
2019-05-24 14:57:40 -0400

    circular queue

--- explore-algorithms/stacks-and-queues/circular_queue.cpp
+++ explore-algorithms/stacks-and-queues/circular_queue.cpp
@@ -4,11 +4,74 @@
 class CircularQueue {
 private:
   std::vector<int> queue;
-  int head, tail;
+  int head, tail, size;
 
 public:
-  CircularQueue() { head = tail = 0; }
+  CircularQueue() {
+    queue.resize(5);
+    head = tail = -1;
+  }
+
   ~CircularQueue() {}
+
+  bool isEmpty() { return queue.size() == 0; }
+
+  bool enqueue(int value) {
+
+    if ((tail - head) == queue.size()) {
+      return false;
+    }
+
+    if (isEmpty()) {
+      head = 0;
+    }
+
+    tail = (++tail % queue.size());
+
+    queue[tail] = value;
+
+    return true;
+  }
+
+  int first() {
+    if (isEmpty()) {
+      return -1;
+    }
+
+    return queue[head];
+  }
+
+  int last() {
+    if (isEmpty()) {
+      return -1;
+    }
+
+    return queue[tail];
+  }
+
+  bool dequeue() {
+    if (isEmpty()) {
+      return false;
+    }
+
+    if (head == tail) {
+      head = tail = -1;
+    }
+
+    head = (++head % queue.size());
+
+    return true;
+  }
 };
 
-int main() { return 0; }
+int main() {
+
+  CircularQueue queue;
+
+  queue.enqueue(5);
+
+  std::cout << queue.first() << std::endl;
+  std::cout << queue.last() << std::endl;
+
+  return 0;
+}
