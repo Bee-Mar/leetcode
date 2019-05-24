031643d3df05b17b2026b1ed75be8ef78e1b973d
Bee-Mar <bpmarlowe-software@protonmail.com>
2019-05-24 17:49:12 -0400

    bfs

---
+++ explore-algorithms/stacks-and-queues/bfs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+
+class BFS {
+ private:
+  std::queue<int> queue;
+  int steps;
+
+ public:
+  BFS() {}
+
+  ~BFS() {}
+};
+
+int main() {
+  BFS bfs;
+
+  return 0;
+}
