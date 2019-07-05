6aeafbfd17438abbc8c66ec01f03b83cb6550f1b
Bee-Mar <bpmarlowe-software@protonmail.com>
2019-07-04 20:07:35 -0400

    longest common prefix

---
+++ challenges/easy/longest_common_prefix/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  string longestCommonPrefix(vector<string> &strs) {
+
+    if (strs.size() == 0) { return ""; }
+    if (strs.size() == 1) { return strs.at(0); }
+
+    char currentChar;
+    int j(0);
+
+    while (j < strs.at(0).size()) {
+      currentChar = strs.at(0).at(j);
+
+      for (int i = 1; i < strs.size(); i++) {
+        if (j >= strs.at(i).size() || strs.at(i).at(j) != currentChar) {
+          return strs.at(0).substr(0, j);
+        }
+      }
+
+      ++j;
+    }
+
+    return strs.at(0).substr(0, j);
+  }
+};
+
+int main(int argc, char *argv[]) {
+
+  Solution solution;
+
+  std::vector<std::vector<std::string>> input{
+      {"flower", "flow", "flight"}, {"dog", "racecar", "car"}, {"c", "c"}, {}};
+
+  for (auto vec : input) { std::cout << solution.longestCommonPrefix(vec) << std::endl; }
+
+  return 0;
+}
---
+++ challenges/easy/longest_common_prefix/runtime.md
@@ -0,0 +1,4 @@
+Runtime: 4 ms, faster than 97.07% of C++ online submissions for Longest Common Prefix.
+Memory Usage: 9 MB, less than 26.36% of C++ online submissions for Longest Common Prefix.
+
+https://leetcode.com/submissions/detail/240833651/
